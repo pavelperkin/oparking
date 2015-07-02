4b0fd0af31a19843b5011a21254facf338dfaa83
pavelperkin <perkinpv@gmail.com>
2015-07-03 00:43:07 +0300

    remove personal data

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Oparking
   class Application < Rails::Application
+    config.time_zone = "Kyiv"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
